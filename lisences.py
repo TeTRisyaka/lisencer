e4bc6451eaacf0d475d8a4d27d80e5a1c327b85a
kirill <tetriskamana@gmail.com>
2025-03-24 16:14:32 +0300

    Initial commit

---
+++ lisences.py
@@ -0,0 +1,72 @@
+
+fsnbProg = {
+  "badge": None,
+  "id": "24",
+  "name": "Программа с ФСНБ-2022",
+  "type": "1",
+  "amount": "3",
+  "duration": "90",
+  "icon": "https://staging-storage.smeta.ru/smetaru/estimate-office/tariffs/gesn_3.png",
+  "status": "1",
+  "custom_license_data": None,
+  "sort": "3",
+  "created_at": "2025-03-20T05:26:12.066Z",
+  "updated_at": "2025-03-20T10:05:56.794Z",
+  "deleted_at": None,
+  "license_data": [
+    {
+      "id": "2",
+      "type": "0",
+      "base_type": None,
+      "name": "Программа",
+      "duration": "30",
+      "cost": "1500.00",
+      "data": {
+        "licenseDuration": 30,
+        "licenseData": [
+          {
+            "applicationType": 0,
+            "licenseDataType": 0
+          }
+        ]
+      },
+      "AvailableLicenseTariff": {
+        "id": "156",
+        "id_available_license": "2",
+        "id_tariff": "24"
+      }
+    },
+    {
+      "id": "20",
+      "type": "2",
+      "base_type": "GESN2022",
+      "name": "ФСНБ-2022",
+      "duration": "30",
+      "cost": "375.00",
+      "data": {
+        "licenseDuration": 30,
+        "licenseData": [
+          {
+            "type": "GESN2022",
+            "baseInfo": [],
+            "licenseDataType": 1
+          },
+          {
+            "type": "GESN2022",
+            "baseInfo": [],
+            "regionCode": None,
+            "yearInfo": [],
+            "quarterInfo": [],
+            "priceZoneInfo": [],
+            "licenseDataType": 3
+          }
+        ]
+      },
+      "AvailableLicenseTariff": {
+        "id": "157",
+        "id_available_license": "20",
+        "id_tariff": "24"
+      }
+    }
+  ]
+}
---
+++ main.py
@@ -0,0 +1,95 @@
+from datetime import datetime
+import requests
+from secrets import login, psw, tokenUrl, url
+
+now = datetime.now().date()
+usrs = 74247, 74598
+
+data = {
+    "login": login,
+    "pass": psw,}
+
+tokenResponse = requests.post(tokenUrl, json=data)
+responceJson = tokenResponse.json()
+token = tokenResponse.json()["token"]
+
+headers = {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+    "authorization": "Bearer" + token,
+    "cache-control": "no-cache",
+    "content-type": "application/json",
+    "dnt": "1",
+    "origin": "https://estimate-office-staging.smeta.ru",
+    "pragma": "no-cache",
+    "priority": "u=1, i",
+    "referer": "https://estimate-office-staging.smeta.ru/eo-admin/user-licenses",
+    "sec-ch-ua": '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"macOS"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
+}
+
+data = {
+    "first": 0,
+    "rows": 15,
+    "sortOrder": 1,
+    "filters": {
+        "id": [{"value": None, "matchMode": "equals", "operator": "and"}],
+        "id_invoice": [{"value": None, "matchMode": "equals", "operator": "and"}],
+        "id_user": [{"value": usrs[1], "matchMode": "equals", "operator": "and"}],
+        "type": [{"value": None, "matchMode": "in", "operator": "and"}],
+        "access_type": [{"value": None, "matchMode": "in", "operator": "and"}],
+        "duration": [{"value": None, "matchMode": "equals", "operator": "and"}],
+        "create_time": [{"value": None, "matchMode": "dateIs", "operator": "and"}],
+        "start_time": [{"value": None, "matchMode": "dateIs", "operator": "and"}],
+        "end_time": [{"value": None, "matchMode": "dateIs", "operator": "and"}],
+        "create_method": [{"value": None, "matchMode": "in", "operator": "and"}],
+        "status": [{"value": None, "matchMode": "in", "operator": "and"}]
+    },
+    "globalFilter": None
+}
+
+response = requests.post(url, headers=headers, json=data)
+
+start = 0
+finish = len(response.json()["rows"])
+licensePool = []
+for item in response.json()["rows"]:
+
+    try:
+        date = response.json()["rows"][start]["end_time"]
+        dt = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%fZ")
+        formatted_date = dt.strftime("%d-%m-%Y")
+    except:
+        formatted_date = "Дата окончания отсутствует"
+
+    try:
+        licenseName = response.json()["rows"][start]["data"][0]["licenseData"][0]["type"]
+       # print(formatted_date + " " + licenseName)
+        licensePool.insert(start, [formatted_date, licenseName])
+        if start < finish - 1:
+            start += 1
+    except:
+      #  print(formatted_date + " Программа")
+        licensePool.insert(start, [formatted_date, "Программа"])
+        if start < finish - 1:
+            start += 1
+
+print(licensePool)
+for elem in licensePool:
+        try:
+            license_date = datetime.strptime(elem[0], "%d-%m-%Y").date()
+            #print(license_date)
+        except:
+            license_date = None
+        try:
+            days_until_license_over = (license_date - now).days
+            if days_until_license_over < 100 and days_until_license_over > 0:
+                print(f'Дней до окончания лицензии {elem[1]} {days_until_license_over}')
+        except:
+           None
+
